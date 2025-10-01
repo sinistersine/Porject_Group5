8e6fa9bf75038dd3c1c30418458f25391cd1e265
Anny Oganesiants <533991@student.fontys.nl>
2025-10-01 16:50:26 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,26 +3,38 @@ import pandas as pd
 import plotly.express as px
 from numpy.random import default_rng as rng
 
+# Set up the data to use it conveniently later on to use for the graphs
+
+
 st.set_page_config(page_title="Bus Scheduling Dashboard", layout="wide")
 st.title("Bus Scheduling & Energy Dashboard")
 st.write(
-    "Upload your bus schedule file below. "
+    "1) Upload your bus schedule file below. "
     "Then switch between the Schedule and Energy tabs to view the cleaned table, "
-  
 )
+
 # run the app with: streamlit run dashboard.py
+
+# FILE UPLOAD
+df_schedule = st.file_uploader(
+    " 1) Upload your bus schedule file (CSV or Excel)",
+    type=["csv", "xlsx"])
+
+
+# THE TABS
 tab_schedule, tab_energy= st.tabs(["Bus schedule", "Energy consumption"])
 
-schedule_file = st.file_uploader(
-    "Upload your bus schedule file (CSV or Excel)",
-    type=["csv", "xlsx"],
-)
-
+# THE ENERGY TAB
 with tab_energy:
     tab_energy.subheader("Example of energy consumption data")
     df = rng(0).standard_normal((10, 1))
     tab_energy.line_chart(df, color="#f178a1")
     
+    
+# TBE SCHEDULE TAB
 with tab_schedule:
     st.header("Bus Schedule")
+    
+    
+    
     
